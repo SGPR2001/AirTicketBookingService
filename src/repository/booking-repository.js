81fc11a073df55c000c9d2f94f7bb5d473def013
SGPR2001 <sgddsp@gmail.com>
2024-01-13 11:38:19 +0530

    added createbooking api

--- src/repository/booking-repository.js
+++ src/repository/booking-repository.js
@@ -18,19 +18,19 @@ class BookingRepository {
       StatusCodes.INTERNAL_SERVER_ERROR);
     }
   }
-  async create(data) {
+  async update(bookingId,data) {
     try {
-      const booking=await Booking.create(data);
-      return booking;
+     const booking=await Booking.findByPk(bookingId)
+     if(data.status){
+      booking.status=data.status;
+     }
+     await booking.save();
+     return booking;
     } catch (error) {
-      if (error.name == "SequelizeValidationError") {
-        console.log("Creating new Validation Error");
-        throw new ValidationError(error);
-      }
-      console.log("Something went wrong on repository layer");
+     
       throw new AppError('RepositoryError',
-      'Cannot create Booking',
-      'There was some issue creating the booking,please try again later',
+      'Cannot update Booking',
+      'There was some issue updating the booking,please try again later',
       StatusCodes.INTERNAL_SERVER_ERROR);
     }
   }
